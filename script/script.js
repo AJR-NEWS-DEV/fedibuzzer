9c46f193f8427bb0976454e6bdc28c26cc1e861e
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-08-03 14:14:40 +0900

    (fix) style

--- script/script.js
+++ script/script.js
@@ -1,17 +1,27 @@
 const design = `<style>
+* {
+    line-height: 1.15;
+    -webkit-tap-highlight-color: transparent;
+}
+
 #__fedishare__ {
     --border-color: #ccc;
     --text-color: #000;
     --button-background-color: #007bff;
     --button-hover-background-color: #0056b3;
     --button-active-background-color: #003d80;
-    display: flex;
-    align-items: center;
     border: 2px solid var(--border-color);
     border-radius: 5px;
     padding: 6px;
+    margin: 0;
     background-color: white;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    container-type: inline-size;
+}
+
+#__fedishare__>div {
+    display: flex;
+    align-items: center;
 }
 
 #__fedishare__.dark {
@@ -48,7 +58,7 @@ const design = `<style>
     color: white;
     border: none;
     border-radius: 5px;
-    padding: .5em 1em;
+    padding: .75em 1.25em;
     font-size: 1rem;
     margin-left: 8px;
     cursor: pointer;
@@ -72,9 +82,9 @@ const design = `<style>
 #__fedishare__ button .i {
     display: inline-block;
     text-transform: none;
-    height: 1rem;
-    width: 1rem;
-    vertical-align: text-top;
+    height: 1em;
+    width: 1em;
+    vertical-align: -.125em;
     margin-right: .25em;
 }
 
@@ -89,8 +99,8 @@ const design = `<style>
     text-underline-offset: 2px;
 }
 
-@media (max-width: 600px) {
-    #__fedishare__ {
+@container (max-width: 450px) {
+    #__fedishare__>div {
         flex-direction: column;
         align-items: stretch;
     }
@@ -105,15 +115,17 @@ const design = `<style>
     }
 }</style>
 <form id="__fedishare__">
-    <input type="text" placeholder="インスタンスのドメイン（例: misskey.io）" />
-    <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="800" height="800" fill="currentColor" class="i" viewBox="-10 -5 1034 1034"><path d="M539 176q-32 0-55 22t-25 55 20.5 58 56 27 58.5-20.5 27-56-20.5-59T544 176h-5zm-87 95L220 389q20 20 25 48l231-118q-19-20-24-48zm167 27q-13 25-38 38l183 184q13-25 39-38zm-142 22L342 585l40 40 143-280q-28-5-48-25zm104 16q-22 11-46 10l-8-1 21 132 56 9zm-426 34q-32 0-55 22.5t-25 55 20.5 58 56.5 27 59-21 26.5-56-21-58.5-55.5-27h-6zm90 68q1 9 1 18-1 19-10 35l132 21 26-50zm225 36-26 51 311 49q-1-8-1-17 1-19 10-36zm372 6q-32 1-55 23t-24.5 55 21 58 56 27 58.5-20.5 27-56.5-20.5-59-56.5-27h-6zm-606 13q-13 25-39 38l210 210 51-25zm-40 38q-21 11-44 10l-9-1 40 256q21-10 45-9l8 1zm364 22 48 311q21-10 44-9l10 1-46-294zm195 23-118 60 8 56 135-68q-20-20-25-48zm26 49L662 856q28 5 48 25l119-231q-28-5-48-25zm-475 29-68 134q28 5 48 25l60-119zm262 17L287 814q19 20 24 48l265-135zm-55 100-51 25 106 107q13-25 39-38zm-291 24q-32 0-55.5 22.5t-25 55 21 57.5 56 27 58.5-20.5 27-56-20.5-58.5-56.5-27h-5zm89 68q2 9 1 18-1 19-9 35l256 41q-1-9-1-18 1-18 10-35zm335 0q-32 0-55 22.5t-24.5 55 20.5 58 56 27 59-21 27-56-20.5-58.5-56.5-27h-6z"></path></svg>Fediverseに共有</button>
+    <div>
+        <input type="text" placeholder="インスタンスのドメイン（例: misskey.io）" />
+        <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="800" height="800" fill="currentColor" class="i" viewBox="-10 -5 1034 1034"><path d="M539 176q-32 0-55 22t-25 55 20.5 58 56 27 58.5-20.5 27-56-20.5-59T544 176h-5zm-87 95L220 389q20 20 25 48l231-118q-19-20-24-48zm167 27q-13 25-38 38l183 184q13-25 39-38zm-142 22L342 585l40 40 143-280q-28-5-48-25zm104 16q-22 11-46 10l-8-1 21 132 56 9zm-426 34q-32 0-55 22.5t-25 55 20.5 58 56.5 27 59-21 26.5-56-21-58.5-55.5-27h-6zm90 68q1 9 1 18-1 19-10 35l132 21 26-50zm225 36-26 51 311 49q-1-8-1-17 1-19 10-36zm372 6q-32 1-55 23t-24.5 55 21 58 56 27 58.5-20.5 27-56.5-20.5-59-56.5-27h-6zm-606 13q-13 25-39 38l210 210 51-25zm-40 38q-21 11-44 10l-9-1 40 256q21-10 45-9l8 1zm364 22 48 311q21-10 44-9l10 1-46-294zm195 23-118 60 8 56 135-68q-20-20-25-48zm26 49L662 856q28 5 48 25l119-231q-28-5-48-25zm-475 29-68 134q28 5 48 25l60-119zm262 17L287 814q19 20 24 48l265-135zm-55 100-51 25 106 107q13-25 39-38zm-291 24q-32 0-55.5 22.5t-25 55 21 57.5 56 27 58.5-20.5 27-56-20.5-58.5-56.5-27h-5zm89 68q2 9 1 18-1 19-9 35l256 41q-1-9-1-18 1-18 10-35zm335 0q-32 0-55 22.5t-24.5 55 20.5 58 56 27 59-21 27-56-20.5-58.5-56.5-27h-6z"></path></svg>Fediverseに共有</button>
+    </div>
 </form>
 <div id="__fedishare_credit__"><a href="https://fedishare.ajr-news.com/" target="_blank" rel="noopener">FediShare - Powered by AJR-NEWS.com</a></div>`;
 
 (() => {
     const scriptOptions = document.currentScript.dataset ?? {};
     const shadowEl = document.createElement('div');
-    const shadow = shadowEl.attachShadow({ mode: 'open' });
+    const shadow = shadowEl.attachShadow({ mode: 'closed' });
     shadow.innerHTML = design;
     const rootEl = shadow.getElementById('__fedishare__');
     if (scriptOptions.color) {
@@ -134,7 +146,7 @@ const design = `<style>
             submitEl.innerHTML = '読み込み中…';
             submitEl.disabled = true;
         }
-        window.fetch(`http://localhost/fedishare/api/v1/instance/${instanceDomain}`).then(async (ctx) => {
+        window.fetch(`https://fedishare-api.ajr-news.com/api/v1/instance/${instanceDomain}`).then(async (ctx) => {
             const content = await ctx.json();
             if (content.status != 200) {
                 switch (content.message.toLowerCase()) {
